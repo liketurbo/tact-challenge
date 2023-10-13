eb304f48970b40a397a94edfa7049394232bab4d
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-10-13 22:04:27 +0300

    Task1 solution

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,3 +1,5 @@
+import "@ton/test-utils";
+
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
 import { Task1 } from '../wrappers/Task1';
@@ -20,14 +22,44 @@ describe('Task1', () => {
                 queryId: 0n,
             }
         );
+        /*
         expect(deployResult.transactions).toHaveTransaction({
             from: deployer.address,
             to: task1.address,
             deploy: true,
             success: true,
         });
+        */
     });
 
-    it('test', async () => {
+    it.skip('initial 0', async () => {
+        let counter = await task1.getCounter();
+        expect(counter).toBe(0n);
     });
+
+    it.skip('add 10', async () => {
+        const dude = await blockchain.treasury('dude');
+        await task1.send(dude.getSender(), {
+            value: toNano('0.05')
+        }, {
+            $$type: 'Add',
+            queryId: 0n,
+            number: 10n
+        });
+        let counter = await task1.getCounter();
+        expect(counter).toBe(10n);
+    })
+
+    it('subtract 10', async () => {
+        const dude = await blockchain.treasury('dude');
+        await task1.send(dude.getSender(), {
+            value: toNano('0.05')
+        }, {
+            $$type: 'Subtract',
+            queryId: 0n,
+            number: 20n
+        });
+        let counter = await task1.getCounter();
+        expect(counter).toBe(-20n);
+    })
 });
